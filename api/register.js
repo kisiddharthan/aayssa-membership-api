7dbca5b5723b18dd8cb4a40e22bb34cde871f821
Siddharthan Kannan Ilangovan <ki.siddharthan@gmail.com>
2026-08-29 00:24:12 -0400

    CORS Fix

--- api/register.js
+++ api/register.js
@@ -1,4 +1,26 @@
 export default async function handler(req, res) {
+
+  // Allow your website to call this API
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://atlantaayyappasevasangam.org"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
+  // Browser preflight request
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       success: false,
